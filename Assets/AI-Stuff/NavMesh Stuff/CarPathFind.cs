8f53d18da3ac782e4b38f2f0deaf5c67863ef3fd
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2022-01-31 13:57:15 +0100

    Navmesh SHITE

--- Assets/AI-Stuff/NavMesh Stuff/CarPathFind.cs
+++ Assets/AI-Stuff/NavMesh Stuff/CarPathFind.cs
@@ -7,19 +7,31 @@ public class CarPathFind : MonoBehaviour
 {
     private NavMeshAgent carAgent;
     public Transform[] checkPoints = new Transform[99];
+    public GameObject[] checkPointsBlockers = new GameObject[99];
     private int checkpointSpot = 0;
     void Start()
     {
         carAgent = GetComponent<NavMeshAgent>();
+        carAgent.updateRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
         carAgent.destination = checkPoints[checkpointSpot].position;
-        //carAgent.SetDestination(checkPoints[checkpointSpot].position);
-        if(carAgent.destination == checkPoints[checkpointSpot].position){
-            checkpointSpot += 1;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Before: " + checkpointSpot);
+
+        if(other.GetComponent<LapHandler>() == true){
+            checkpointSpot = 0;
         }
+
+        checkpointSpot = other.GetComponent<Checkpoint>().Index+1;
+        checkPointsBlockers[checkpointSpot-1].SetActive(true);
+        Debug.Log("After: " + checkpointSpot);
     }
 }
